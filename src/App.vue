d9473468ce9ec048cbabca4a9aa438d15aec9e36
Eugen Istoc <eugenistoc@gmail.com>
2025-03-05 07:02:28 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']"/>
+       yo dawg
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
