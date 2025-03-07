2a59082f48f072d76ef610102364a215220adca1
Eugen Istoc <eugenistoc@gmail.com>
2025-03-07 12:56:11 -0500

    combine posts with authors in index.vue; display author names or 'No Author' if not found

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,17 +2,37 @@
   import { useData } from "@/composables"
   import { useDialog, useOverlay } from "@/composables/ui"
   import EditPostDialog from "@/components/dialogs/EditPost.vue"
+  import { computed } from "vue"
 
-  const { all, create, remove } = useData()
+  const { all, remove } = useData()
   const { create: createOverlay } = useOverlay()
   const { confirm } = useDialog()
 
   const { data: posts, refetch } = all("posts")
+  const { data: authors } = all("authors")
 
   const editPostDialog = createOverlay(EditPostDialog)
 
+  /**
+   * Combine posts with authors usnig 'authorId'. If author is not found, display 'No Author'
+   *
+   * Normally, this expantion would be done in the backend, but for the sake of this example, we are doing it here.
+   */
+
+  const postsWithAuthors = computed(() => {
+    if (!posts.value || !authors.value) return []
+
+    return posts.value.map((post) => {
+      const author = authors.value.find((author) => author.id === post.authorId)
+
+      return { ...post, author: author ? `${author.firstName} ${author.lastName}` : "No Author" }
+    })
+  })
+
   const onCreate = async () => {
-    editPostDialog.open()
+    const createdPost = await editPostDialog.open()
+
+    if (createdPost) await refetch()
   }
 
   const onDelete = async (id: number) => {
@@ -25,7 +45,9 @@
   }
 
   const onEdit = async (id: number) => {
-    editPostDialog.open({ id })
+    const editedPost = await editPostDialog.open({ id })
+
+    if (editedPost) await refetch()
   }
 </script>
 
@@ -37,8 +59,8 @@
     </div>
     <v-divider />
 
-    <v-container class="px-0 py-5" fluid v-for="post in posts" :key="post.id">
-      <p class="font-weight-medium text-primary">{{ post.authorId }}</p>
+    <v-container class="px-0 py-5" fluid v-for="post in postsWithAuthors" :key="post.id">
+      <p class="font-weight-medium text-primary">{{ post.author }}</p>
       <div class="d-flex justify-space-between align-center">
         <p class="mt-2 text-h5 font-weight-bold text-sm-h4">{{ post.title }}</p>
         <div class="d-flex ga-2 px-2">
